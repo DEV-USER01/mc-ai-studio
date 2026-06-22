b1fc656d18b77754a820cbde0709daae7899bafc
DEV-USER01 <gamekingthailand@gmail.com>
2026-06-23 00:02:36 +0700

    Add function to detect pack types from modules

--- js/pack-type-detector.js
+++ js/pack-type-detector.js
@@ -1,1 +1,20 @@
+function detectPackType(modules = []) {
 
+    if (modules.some(m => m.type === "resources")) {
+        return "Resource Pack";
+    }
+
+    if (modules.some(m => m.type === "data")) {
+        return "Behavior Pack";
+    }
+
+    if (modules.some(m => m.type === "world_template")) {
+        return "World Template";
+    }
+
+    if (modules.some(m => m.type === "skin_pack")) {
+        return "Skin Pack";
+    }
+
+    return "Unknown";
+}
