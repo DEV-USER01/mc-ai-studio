3eb64fbe1cd9f8bc2d3dbf42cf9d00223f9c9c29
DEV-USER01 <gamekingthailand@gmail.com>
2026-06-23 01:21:01 +0700

    Add getBlockList function to retrieve block names

--- js/block-reader.js
+++ js/block-reader.js
@@ -1,6 +1,21 @@
 function countBlocks(files) {
+
     return files.filter(file =>
         file.includes("blocks/") &&
         file.endsWith(".json")
     ).length;
+
+}
+
+function getBlockList(files) {
+
+    return files
+        .filter(file =>
+            file.includes("blocks/") &&
+            file.endsWith(".json")
+        )
+        .map(file =>
+            file.split("/").pop().replace(".json", "")
+        );
+
 }
