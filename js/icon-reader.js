2e6186b50b858715ed7ae3a50e082a4916a09900
DEV-USER01 <gamekingthailand@gmail.com>
2026-06-23 00:47:45 +0700

    Add async function to extract pack icon from zip

--- js/icon-reader.js
+++ js/icon-reader.js
@@ -1,1 +1,14 @@
+async function extractPackIcon(zip) {
 
+    const iconFile = zip.file("pack_icon.png");
+
+    if (!iconFile) {
+        return null;
+    }
+
+    const blob = await iconFile.async("blob");
+
+    return URL.createObjectURL(blob);
+}
+
+window.extractPackIcon = extractPackIcon;
