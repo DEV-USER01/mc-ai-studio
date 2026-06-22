d27aacc1c33f5d430a53327d40864369032af7c0
DEV-USER01 <gamekingthailand@gmail.com>
2026-06-22 23:51:48 +0700

    Refactor checkFileInfo function for readability

--- js/file-checker.js
+++ js/file-checker.js
@@ -1,4 +1,5 @@
 function checkFileInfo(file) {
+
     if (!file) {
         return {
             valid: false,
@@ -9,8 +10,14 @@ function checkFileInfo(file) {
     const name = file.name.toLowerCase();
 
     return {
-        valid: name.endsWith(".mcpack") || name.endsWith(".mcaddon"),
+        valid:
+            name.endsWith(".mcpack") ||
+            name.endsWith(".mcaddon"),
+
         name: file.name,
+
         size: (file.size / 1024).toFixed(2)
     };
 }
+
+window.checkFileInfo = checkFileInfo;
