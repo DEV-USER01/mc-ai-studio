d51714478d376419f52c182a335171bcfa7570b3
DEV-USER01 <gamekingthailand@gmail.com>
2026-06-22 23:35:40 +0700

    Add async function to read manifest from zip

--- js/manifest-reader.js
+++ js/manifest-reader.js
@@ -1,1 +1,12 @@
+async function readManifest(zip) {
 
+    const manifestFile = zip.file("manifest.json");
+
+    if (!manifestFile) {
+        return null;
+    }
+
+    const text = await manifestFile.async("string");
+
+    return JSON.parse(text);
+}
