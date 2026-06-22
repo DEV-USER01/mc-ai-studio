07ac727d929f0b5defbf8a2ecb626afdbc5304e4
DEV-USER01 <gamekingthailand@gmail.com>
2026-06-22 23:35:14 +0700

    Add file validation function for .mcpack and .mcaddon

--- js/file-checker.js
+++ js/file-checker.js
@@ -1,1 +1,16 @@
+function checkFile(file) {
+    if (!file) {
+        return {
+            valid: false,
+            message: "ไม่ได้เลือกไฟล์"
+        };
+    }
 
+    const name = file.name.toLowerCase();
+
+    return {
+        valid: name.endsWith(".mcpack") || name.endsWith(".mcaddon"),
+        name: file.name,
+        size: (file.size / 1024).toFixed(2)
+    };
+}
