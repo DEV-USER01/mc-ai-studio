04a7b51cd180f466e5319abe228f9477c6edfb29
DEV-USER01 <gamekingthailand@gmail.com>
2026-06-23 00:03:24 +0700

    Add generateReport function to generate a report

--- js/report-generator.js
+++ js/report-generator.js
@@ -1,1 +1,24 @@
+function generateReport(data) {
 
+    return `
+=== MC AI Studio Report ===
+
+Pack:
+${data.name}
+
+Version:
+${data.version}
+
+Type:
+${data.type}
+
+UUID:
+${data.uuid}
+
+Score:
+${data.score}/100
+
+Issues:
+${data.issues.join("\n") || "None"}
+`;
+}
