c7113cb034d25f57d25d4a8446904f148f13c97a
DEV-USER01 <gamekingthailand@gmail.com>
2026-06-23 01:20:37 +0700

    Add getItemList function to retrieve item names

--- js/item-reader.js
+++ js/item-reader.js
@@ -1,6 +1,21 @@
 function countItems(files) {
+
     return files.filter(file =>
         file.includes("items/") &&
         file.endsWith(".json")
     ).length;
+
+}
+
+function getItemList(files) {
+
+    return files
+        .filter(file =>
+            file.includes("items/") &&
+            file.endsWith(".json")
+        )
+        .map(file =>
+            file.split("/").pop().replace(".json", "")
+        );
+
 }
