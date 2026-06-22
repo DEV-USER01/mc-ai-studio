0dcb4b6f704ec2a51d9a9d45a41425f9c5c83260
DEV-USER01 <gamekingthailand@gmail.com>
2026-06-23 01:20:09 +0700

    Add countEntities function to count entity files
    Added a function to count JSON entity files.

--- js/entity-reader.js
+++ js/entity-reader.js
@@ -1,7 +1,21 @@
+function countEntities(files) {
+
+    return files.filter(file =>
+        file.includes("entities/") &&
+        file.endsWith(".json")
+    ).length;
+
+}
+
 function getEntityList(files) {
 
     return files
-        .filter(f => f.includes("entities/"))
-        .map(f => f.split("/").pop().replace(".json",""));
+        .filter(file =>
+            file.includes("entities/") &&
+            file.endsWith(".json")
+        )
+        .map(file =>
+            file.split("/").pop().replace(".json", "")
+        );
 
 }
