54cbe59a29d4c401026829e4bfef4a7937167c23
DEV-USER01 <gamekingthailand@gmail.com>
2026-06-22 23:20:42 +0700

    Add analyzePack function to classify file packs

--- js/js/js/ai-analyzer.js
+++ js/js/js/ai-analyzer.js
@@ -1,1 +1,13 @@
+function analyzePack(files){
 
+    if(files.some(f=>f.startsWith("texts/")))
+        return "Language Pack";
+
+    if(files.some(f=>f.startsWith("textures/")))
+        return "Resource Pack";
+
+    if(files.some(f=>f.startsWith("entities/")))
+        return "Behavior Pack";
+
+    return "Unknown";
+}
