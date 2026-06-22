2dbd5ae2a360be0f2eb917d64393488671d91e3e
DEV-USER01 <gamekingthailand@gmail.com>
2026-06-23 01:22:48 +0700

    Enhance countSounds to include .ogg and .fsb files

--- js/sound-reader.js
+++ js/sound-reader.js
@@ -1,5 +1,11 @@
 function countSounds(files) {
+
     return files.filter(file =>
-        file.includes("sounds/")
+
+        file.includes("sounds/") ||
+        file.endsWith(".ogg") ||
+        file.endsWith(".fsb")
+
     ).length;
+
 }
